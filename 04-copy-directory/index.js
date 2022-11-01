c24992f78ba58638a8943c43dc4aac6d40248e91
Tatiana Ivanova <tat.mi.iva@gmail.com>
2022-11-01 10:22:07 +0300

    feat: add solution for the task-4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+const fs = require('fs');
+const path = require('path');
 
+const { stdout } = process;
+
+function copyDir() {
+  makeDir();
+  let copiedFiles = [];
+  fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+    copiedFiles = files;
+    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+      copiedFiles.forEach(file => {
+        if (!files.includes(file)) {
+          fs.rm(path.join(__dirname, 'files-copy', file), err => {
+            if (err) stdout.write(err);
+          })
+        }
+      })
+      files.forEach(file => {
+        fs.copyFile(
+          path.join(__dirname, 'files', file),
+          path.join(__dirname, 'files-copy', file),
+          err => {
+            if (err) throw err;
+          }
+          )
+      });
+    });
+  })
+}
+
+copyDir();
+
+function makeDir() {
+  fs.mkdir(
+    path.join(__dirname, 'files-copy'),
+    { recursive: true },
+    err => {
+      if (err) throw err;
+    }
+  );
+}
